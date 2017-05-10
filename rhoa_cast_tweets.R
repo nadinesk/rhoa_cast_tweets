a2bb1ad74adc39b7ee9e05702f5ec9c381d2f8d2
nadinesk <nadine.khattak@gmail.com>
2017-05-10 13:06:26 -0400

    updates and charts

--- rhoa_cast_tweets.R
+++ rhoa_cast_tweets.R
@@ -11,7 +11,7 @@ library(reshape2)
 library(scales)
 
 
-setup_twitter_oauth()
+setup_twitter_oauth("YzDrMmjsdPajxmuMXGrfZz2zB", "fDK3UJOuUnp1OF8Dd91RSsFcKeB1tQ53W9FKoqxu7mKcY0efQh", "499964206-UwcPxJM1hr2iQ0aTAgUev8hdOptejKkNkBmrn2Nu", "785vZOkgTGramES0HsjJ7TBFibUhjs274KL5XxdlPjnfC")
 
 kb <- userTimeline("kandi", n= 3200)
 
@@ -49,6 +49,8 @@ kb4$who <- "kb"
 
 kb_count <- count(kb1)
 
+kb4
+
 ###
 
 
@@ -79,13 +81,13 @@ pp4$perc <- pp4$words/(sum(pp4$words))
 
 sum(pp4$perc)
 
-
+pp4
 
 
 #####################
 pw <- userTimeline("Porsha4real", n= 3200)
 
-pw1 <- do.call("rbind", lapwly(pw, as.data.frame))
+pw1 <- do.call("rbind", lapply(pw, as.data.frame))
 
 pw2 <- pw1[c(1)]
 
@@ -203,15 +205,19 @@ sum(km4$perc)
 km4$who <- "km"
 
 jn <- kb4 %>%
-        inner_join(pp4, by="sentiment") %>%
-        inner_join(pw4, by="sentiment") %>%
-        inner_join(sw4, by="sentiment") %>%
-        inner_join(cb4, by="sentiment") %>%
-        inner_join(km4, by="sentiment") 
+        left_join(pp4, by="sentiment") %>%
+        left_join(pw4, by="sentiment") %>%
+        left_join(sw4, by="sentiment") %>%
+        left_join(cb4, by="sentiment") %>%
+        left_join(km4, by="sentiment") 
+
+jn
 
 jn1 <- jn[c(1,3,6,9,12,15,18)]
 
 str(jn1)
+
+jn1
 
 names(jn1)[2] <- "kb"
 names(jn1)[3] <- "pp"
@@ -221,7 +227,11 @@ names(jn1)[6] <- "cb"
 names(jn1)[7] <- "km"
 
 jn3 <- melt(jn1, id=c('sentiment'))
+
 jn3
+
+jn3[is.na(jn3)] <- 0
+
 
 setwd("D:/dv")
 ggplot(jn3, aes(x=sentiment, y=value)) + 
@@ -247,6 +257,57 @@ dev.copy(png, file = "members_facet.png", width=800)
 
 dev.off()
 
+
+#############
+
+jn4 <- jn3
+
+jn4
+
+jn4$sentiment <- gsub("^negative$", "negative", jn4$sentiment)  #
+jn4$sentiment <- gsub("^positive$", "positive", jn4$sentiment) #
+jn4$sentiment <- gsub("^anger$", "negative", jn4$sentiment) #
+jn4$sentiment <- gsub("^sadness$", "negative", jn4$sentiment) #
+jn4$sentiment <- gsub("^fear$", "negative", jn4$sentiment) #
+jn4$sentiment <- gsub("^trust$", "positive", jn4$sentiment) #
+jn4$sentiment <- gsub("^disgust$", "negative", jn4$sentiment) #
+jn4$sentiment <- gsub("^anticipation$", "positive", jn4$sentiment) #
+jn4$sentiment <- gsub("^joy$", "positive", jn4$sentiment) #
+jn4$sentiment <- gsub("^surprise$", "positive", jn4$sentiment)
+
+jn4$value <- as.numeric(jn4$value)
+
+jn5 <- jn4 %>%
+  group_by(sentiment, variable) %>%
+  summarise_each(funs(sum)) %>%
+  arrange(variable, sentiment)
+
+jn5
+
+
+ggplot(jn5, aes(x=sentiment, y=value, label=variable, fill=variable)) + 
+  geom_bar( stat="identity", position=position_dodge()) + 
+  theme(axis.text.x = element_text(angle=45, hjust=1)) + 
+geom_text(aes(label=variable), colour="black", vjust=1.5, position=position_dodge(0.9), size=4)
+
+
+
+dev.copy(png, file = "sent_group.png", width=800)  
+
+dev.off()
+
+jn6 <- jn5 %>%
+        filter(sentiment == 'positive')
+
+
+jn6
+
+mean(jn6$value)
+
+jn7 <- jn5 %>%
+  filter(sentiment == 'negative')
+
+mean(jn7$value)
 
 #kb
 #pp
@@ -423,6 +484,8 @@ stat <- rbind(kb_stat, pp_stat, pw_stat, cb_stat, km_stat, sw_stat)
 
 stat
 
+mean(stat$words_incl_perc)
+
 
 ggplot(stat, aes(x=who, y=tweet_count)) + 
   geom_bar(aes(fill=who), stat="identity", position="dodge") + 
@@ -437,7 +500,7 @@ dev.off()
 #   geom_bar(aes(fill=who), stat="identity", position="dodge") + 
 #   theme(axis.text.x = element_text(angle=45, hjust=1)) 
 
-
+jn3
 
 ggplot(stat, aes(x=who, y=words_included)) + 
   geom_bar(aes(fill=who), stat="identity", position="dodge") + 
